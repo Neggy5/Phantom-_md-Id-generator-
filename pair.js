b5b3bd7739a1e1974f91730116423ef09dca8123
Neggy5 <132104247+Neggy5@users.noreply.github.com>
2025-04-24 05:47:59 +0100

    Update pair.js

--- pair.js
+++ pair.js
@@ -61,17 +61,16 @@ router.get('/', async (req, res) => {
 *_Made With 🤍_*
 ______________________________________
 ╔════◇
-║ *『 WOW YOU'VE CHOSEN WASI MD 』*
+║ *『 WOW YOU'VE CHOSEN PHANTOM MD 』*
 ║ _You Have Completed the First Step to Deploy a Whatsapp Bot._
 ╚════════════════════════╝
 ╔═════◇
 ║  『••• 𝗩𝗶𝘀𝗶𝘁 𝗙𝗼𝗿 𝗛𝗲𝗹𝗽 •••』
-║❒ *Ytube:* _youtube.com/@wasitech1_
-║❒ *Owner:* _https://wa.me/923192173398_
-║❒ *Repo:* _https://github.com/wasixd/WASI-MD
-║❒ *WaGroup:* _https://whatsapp.com/channel/0029VaDK8ZUDjiOhwFS1cP2j
-║❒ *WaChannel:* _https://whatsapp.com/channel/0029VaDK8ZUDjiOhwFS1cP2j
-║❒ *Plugins:* _https://github.com/wasixd/WASI-MD-PLUGINS_
+║❒ *Ytube:* _youtube.com/@BOTKING_
+║❒ *Owner:* _https://wa.me/2349079055953_
+║❒ *Repo:* _https://github.com/Neggy5/PHANTOM_MD
+║❒ *WaChanell:* _https://whatsapp.com/channel/0029VaySwxF9Bb67U6rkUB2i
+║❒ *Plugins:* _https://github.com/wasixd/PHANTOM_MD-PLUGINS_
 ╚════════════════════════╝
 _____________________________________
 
